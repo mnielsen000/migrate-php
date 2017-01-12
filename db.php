18e7ed5b703c72ad7a9346bbcbc8dee14dbe1852
root <root@VA1COSGPHPMIG08.local>
2017-01-12 05:42:55 +0000

    db changes

--- db.php
+++ db.php
@@ -4,14 +4,23 @@ class Database {
 	
 	public function query($query) {
 
-		$db_host = "%%DB_HOST%%";
-		$db_user = "%%DB_USER%%";
-		$db_password = "%%DB_PASSWORD%%";
-		$db_name = "%%DB_NAME%%"
+		$db_host = "206.128.152.47";
+		$db_user = "root";
+		$db_password = "";
+		$db_name = "migratedemo"
 
 		$con = mysql_connect($db_host, $db_user, $db_password) or die(mysql_error());
-		$connect = mysql_select_db($db_name, $con) or die(mysql_error());
-		$this->result = mysql_query($query, $j) or die(mysql_error());
+
+		mysql_query("CREATE DATABASE IF NOT EXISTS " . $db_name,NULL) or die(mysql_error());
+
+
+                //$connect = mysql_select_db($db_name, $con) or die(mysql_error());
+
+                //mysql_query("USE sampledb; CREATE TABLE IF NOT EXISTS users (first_name VARCHAR(20),  last_name VARCHAR(20));", $con) or die(mysql_error());
+
+
+		//$connect = mysql_select_db($db_name, $con) or die(mysql_error());
+		//$this->result = mysql_query($query, $j) or die(mysql_error());
 		return $this->result;
 	}
 
--- dbtest.php
+++ dbtest.php
@@ -1,24 +1,26 @@
 <?php 
 
-include 'db_connect.php';  //include the db_connect.php file
+include 'db.php';  //include the db_connect.php file
 
 print "<HTML><BODY><H1>User in the database:!</H1>\n";
 
 
-CREATE DATABASE IF NOT EXISTS DBName;
+//Create and set a new connection
+$get = new Database();
 
 //Create a query: $var->query("SQL_STATEMENT"); $var is the variable you created a connection with.
-$selectusers = $get->query("SELECT * FROM users");
+//$selectusers = $get->query("SELECT * FROM users");
 
 //Do whatever you would like with the query
 print "Total Number of user records: ";
-print mysql_num_rows($selectusers);
+//print mysql_num_rows($selectusers);
 print "\n\r";
 
 //Use built in functions to make one line queries.
-$rowarray = $get->fetch($selectusers);  //fetch() fetches the associated rows.
+//$rowarray = $get->fetch($selectusers);  //fetch() fetches the associated rows.
 print "<table>\n\r";  
 
+/*
 foreach ($rowarray as $row) {
     print "<tr>\n\r";  
     foreach ($row as $col) {
@@ -26,7 +28,7 @@ foreach ($rowarray as $row) {
     }
     print "</tr>\n\r";
 }
-
+*/
 print "</table></BODY></HTML>";
 
 ?>
