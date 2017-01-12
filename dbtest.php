fbb869c1a8d114ecb55a8c2eb2a7bddb0fb5593a
root <root@VA1COSGPHPMIG08.local>
2017-01-12 23:19:04 +0000

    fixed dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -2,33 +2,51 @@
 
 include 'db.php';  //include the db_connect.php file
 
-print "<HTML><BODY><H1>User in the database:!</H1>\n";
+print "<HTML><BODY><H1>Users in the database:</H1>\n";
 
 
 //Create and set a new connection
 $get = new Database();
 
-//Create a query: $var->query("SQL_STATEMENT"); $var is the variable you created a connection with.
-//$selectusers = $get->query("SELECT * FROM users");
+$firstname = htmlspecialchars($_POST["firstname"]);
+$lastname = htmlspecialchars($_POST["lastname"]);
+
+if ( $firstname && $lastname )
+{
+  $get->query("insert into users (first_name, last_name) values (\"$firstname\",\"$lastname\")");
+}
+
+$selectusers = $get->query("SELECT * FROM users");
 
 //Do whatever you would like with the query
 print "Total Number of user records: ";
-//print mysql_num_rows($selectusers);
-print "\n\r";
+print mysql_num_rows($selectusers);
+print "<br/><br/>";
 
 //Use built in functions to make one line queries.
-//$rowarray = $get->fetch($selectusers);  //fetch() fetches the associated rows.
+$rowarray = $get->fetch($selectusers);  //fetch() fetches the associated rows.
 print "<table>\n\r";  
 
-/*
 foreach ($rowarray as $row) {
-    print "<tr>\n\r";  
-    foreach ($row as $col) {
-        print "\t<td>$col</td>\n\r";
-    }
-    print "</tr>\n\r";
+	print "<tr>\n\r";  
+    	foreach ($row as $col) {
+        	print "\t<td>$col</td>\n\r";
+    	}
+    	print "</tr>\n\r";
 }
-*/
+
+print "</table><br/>Add User:<br/><br/>"
+?>
+
+<form method="post" action="dbtest.php">
+  <input type="text" name="firstname" id="firstname" maxlength="20">
+  <input type="text" name="lastname" id="lastname" maxlength="20">
+  <input type="submit" value="submit">
+</form>
+
+<?php
+
+
 print "</table></BODY></HTML>";
 
 ?>
